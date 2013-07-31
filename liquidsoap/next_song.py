d80eacf8f4e27ef1e2be352fffd2e185993e80e6
Rafael Korbas <rafael.korbas@gmail.com>
2013-07-31 14:57:43 +0200

    pridane nejake testy na generator playlistu

--- liquidsoap/next_song.py
+++ liquidsoap/next_song.py
@@ -1,6 +1,7 @@
 import random
 
 fname = "playlist.m3u"
+playlist_length = 5
 
 """
     returns path to next song to play
@@ -11,7 +12,8 @@ def pick_next_song():
 """
     fills playlist with next 5 files to play
 """
-def generate_next_song():
+def generate_next_song(fname = fname):
+    print(fname)
     songs = []
     try:
         with open(fname) as f:
@@ -19,7 +21,7 @@ def generate_next_song():
             songs.pop(0) # vrchnu pesnicku zmazeme (to je ta, co sa aktualne prehrava)
     except:
         print("Failed to open " + fname)
-    while (len(songs) < 5):
+    while (len(songs) < playlist_length): 
         songs.append(pick_next_song() + "\n")
         
     file = open(fname, "w") # writes list with songs to playlist file
