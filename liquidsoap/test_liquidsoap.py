49265e70f03baf2e0f2ee1cf33c0f9d898287ab7
Rafael Korbas <rafael.korbas@gmail.com>
2013-07-31 14:12:08 +0200

    pridany naznak testov pre skripty k liquidsoap

---
+++ liquidsoap/test_liquidsoap.py
@@ -0,0 +1,11 @@
+import unittest
+import feedback
+import next_song
+
+class SimplisticTest(unittest.TestCase):
+
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
