1350004c3435ad62f63214cf4209e6b1ec587ba9
Radoslav Blecha <r3dr1f@gmail.com>
2013-08-01 14:53:42 +0200

    Premenovanie funkcie v scripts.js

--- project/project/static/js/scripts.js
+++ project/project/static/js/scripts.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 
-$(".trigger").click(function() {
-	$('.default_hide').slideToggle("5000");
-});
+function registracia_objavenie_mena_interpreta(){
+	$(".trigger").click(function() {
+		$('.default_hide').slideToggle("fast");
+	});
+}
+
+registracia_objavenie_mena_interpreta();
+
 
 });
