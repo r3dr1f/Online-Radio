7d915ffb4bfe0353963c08340b350eee6b70d3da
Rafael Korbas <rafael.korbas@gmail.com>
2013-08-13 10:56:53 +0200

    fixnuty server (predtym padal na feedback.py pri databaze)

--- project/project/liquidsoap/feedback.py
+++ project/project/liquidsoap/feedback.py
@@ -11,26 +11,33 @@ import datetime
 
 
 def update_factor_played():
-	songs = _session.query(Song).all()
-	for song in songs:
-		song.current_rating = (9 * song.current_rating + song.rating_max * song.factor_age) / 10
-		_session.add(song)
-	_session.commit()
-	
+	try:
+		songs = _session.query(Song).all()
+		for song in songs:
+			song.current_rating = (9 * song.current_rating + song.rating_max * song.factor_age) / 10
+			_session.add(song)
+		_session.commit()
+	except:
+		print("Failed to update song ratings")
+		
 	#vynulujeme rating songom v playliste, co sa este len chystaju prehrat, ale vieme, ze sa prehraju
-	songs_to_be_played = _session.query(Song).join(Playlist).filter(Playlist.play_time == None).all()
-	#print(songs_to_be_played)
-	for song in songs_to_be_played:
-		song.current_rating = 0
-		_session.add(song)
-	_session.commit()
-	
+	try:
+		songs_to_be_played = _session.query(Song).join(Playlist).filter(Playlist.play_time == None).all()
+		#print(songs_to_be_played)
+		for song in songs_to_be_played:
+			song.current_rating = 0
+			_session.add(song)
+		_session.commit()
+	except:
+		print("failed to set song ratings to 0")
 	#vynulujeme rating prave prehravanemu songu
-	cur_song = _session.query(Song).filter(Song.id == sys.argv[1]).first()
-	cur_song.current_rating = 0
-	_session.add(cur_song)
-	_session.commit()
-
+	try:
+		cur_song = _session.query(Song).filter(Song.id == sys.argv[1]).first()
+		cur_song.current_rating = 0
+		_session.add(cur_song)
+		_session.commit()
+	except:
+		print("failed to update currently playing song rating to 0")
 
 #-init means cleaning database playlist from queued but not played songs
 if (sys.argv[1] == "-init"):
